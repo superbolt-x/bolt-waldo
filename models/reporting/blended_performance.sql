6a2dacf37bdab6fe0b637623ebd585157016f6fe
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-03-28 16:41:32 -0400

    Update blended_performance.sql

--- models/reporting/blended_performance.sql
+++ models/reporting/blended_performance.sql
@@ -10,7 +10,7 @@ SUM(coalesce(purchases,0)) as paid_purchase,
 SUM(coalesce(revenue,0)) as paid_revenue,
 SUM(coalesce(link_clicks,0)) as clicks,
 SUM(coalesce(impressions,0)) as impressions
-from reporting.waldo_facebook_ad_performance
+FROM {{ source('reporting','facebook_ad_performance') }}
 group by 1,2),
 
 google as (select date::date, date_granularity,
@@ -20,7 +20,7 @@ SUM(coalesce(purchases,0)) as paid_purchase,
 SUM(coalesce(revenue,0)) as paid_revenue,
 SUM(coalesce(clicks,0)) as clicks,
 SUM(coalesce(impressions,0)) as impressions
-from reporting.waldo_googleads_campaign_performance
+FROM {{ source('reporting','googleads_campaign_performance') }}
 group by 1,2)
 
 select *
